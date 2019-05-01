9ea3ffa970149f3064773a2b465567a776aad5de
Devyn Theis <dtheis2@illinois.edu>
2019-05-01 14:19:18 -0500

    Updated UI

--- app_ui.py
+++ app_ui.py
@@ -11,41 +11,67 @@ from tkinter import *
 #
 ###############################################################################
 
-def get_tuples():
-    # would perform the search here
-    links = ["<put links here>", "https://www.coursera.org/learn/text-mining/lecture/8Ki0H/4-4-text-clustering-generative-probabilistic-models-part-3"]
-    text = ["<text results go here>", "and what the likelihood function looks like. And we can also compute the maximum likelihood estimate, to estimate the parameters. In this lecture, we're going to do talk more about how exactly we're going to compute the maximum likelihood estimate. As in most cases the Algorithm can be used to solve this problem for mixture models. So here's the detail of this Algorithm for document clustering. Now, if you have understood how Algorithm works for topic models like TRSA, and I think here it would be very similar. And we just need to adapt a little bit to this new mixture model. So as you may recall Algorithm starts with initialization of all the parameters. So this is the same as what happened before for topic models."]
 
-    results = []
-    for i in range(len(links)):
-      results.append((links[i], text[i]))
+# global list to hold active result tkinter widgets
+active_result_widgets = []
 
-    return results
+def clear_current_results():
+    # clear old tkinter result widgets on the screen
 
-# Triggered upon hitting the 'submit' button in the UI
+    global active_result_widgets
+    for result in active_result_widgets:
+        for obj in result:
+            obj.destroy()
+
+    active_result_widgets.clear()
+
+def add_results_to_gui(tup, row_count):
+    # place each result tuple onto gui window
+    # each result tuple displays the video link for reference, text
+    # search results, and associated timestamp
+
+    link = Label(root, text=tup[0], fg="blue")
+    link.grid(row=row_count, column=1,pady=(25,0))
+    row_count += 4
+
+    txt = Text(root, height=4, width=100)
+    txt.insert(INSERT, tup[2])
+    txt.grid(row=row_count, column=1)
+
+    ts = Label(root, text=tup[1])
+    ts.grid(row=row_count, column=0)
+    row_count += 4
+
+    result_objs = [link, txt, ts]
+    active_result_widgets.append(result_objs)
+
+    return row_count
+
 def submit():
+    # clicking submit button triggers this function
 
-    # we can get the users keywords they entered like this:
+    # fetch user input (keywords) and number of requested results
     user_input = search_box.get()
     num_results = int(v.get())
 
+    # perform search
     result = app.search_query(user_input, num_results)
 
+    # clear any leftover results on gui screen before displaying new results
+    clear_current_results()
+
+    # add "results" header onto Gui
     Label(root, text="Results:").grid(row=2)
 
-    prev = 4  # start at grid row 4
+    row_count = 4 # start at grid row 4 (when displaying results)
+
     for tup in result:
-        Label(root, text=tup[0], fg="blue").grid(row=prev, column=1,pady=(25,0))
-        prev = prev+4
-        result = Text(root, height=4, width=100)
-        result.insert(INSERT, tup[2])
-        result.grid(row=prev, column=1)
-        Label(root, text=tup[1]).grid(row=prev, column=0)
-        prev= prev + 4
+        row_count = add_results_to_gui(tup, row_count)
 
 
 if __name__ == '__main__':
-    # this is the main window frame to build off of
+
+    # this is the main window object of the gui
     root = tkinter.Tk()
 
     # add search / text entry box to take in user inputs
@@ -54,11 +80,12 @@ if __name__ == '__main__':
     search_box.grid(row=0, column=1)
     Button(root, command=submit, text="Submit").grid(row=0, column=3)
 
-    # option meaning
+    # add OptionMenu to offer user selection of number of results
     my_options = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
     v = StringVar()
     v.set("5")
     om = OptionMenu(root, v, *my_options)
     om.grid(row=0, column=2)
 
+    # start gui
     root.mainloop()
