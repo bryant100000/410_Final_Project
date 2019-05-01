7acc03de50abcce244913cce05b86d832c9fa49b
Bryant <bryant100000@gmail.com>
2019-04-30 20:12:53 -0500

    Added documentation; awaiting app_ui.py updates (and further documentation).

--- app.py
+++ app.py
@@ -4,30 +4,40 @@ from sklearn.neighbors import NearestNeighbors
 import numpy as np
 import matplotlib.pyplot as plt
 
+###############################################################################
+#
+# App:
+#   Back-end of the application. Performs the pre-processing, document
+#   representation, stop word removal/normalization, and search functions.
+#   To change representation, the bag-of-words unigram model, KNN, and stop
+#   word removal thresholds can be modified.
+#
+###############################################################################
+
+# Clean link data for usage, strip ending char
 filename = "Lecture Video Links.txt"
 with open(filename,"r") as f:
     lines = f.readlines()
-    #print(lines)
     for i in range(len(lines)):
         lines[i] = lines[i].replace("\n","")
 lines[0] = "https://www.coursera.org/lecture/text-retrieval/lesson-1-1-natural-language-content-analysis-rLpwp"
-#pprint(lines)
 
+# Pre-process transcript data into text, video #, timestamp
 data_tuples = []
 data_text = []
 lecture_video = -1
 with open("data_text.txt","r") as f:
   file = f.readlines()
   for i in range(len(file)):
-      split_text = file[i].split(":",2)
+      split_text = file[i].split(":",2) # delimit format
       timestamp = ":".join(split_text[:2])
       if timestamp == "0:00":
-          lecture_video += 1
+          lecture_video += 1 # indexing for future reference
       text = split_text[2].replace(" \n", "").lower()
       data_text.append(text)
       data_tuples.append((lines[lecture_video],timestamp,text))
-  #pprint(data_tuples[0:60])
 
+# Search function for direct term match
 def search(term):
     term = term.lower()
     found_tuples = []
@@ -36,32 +46,35 @@ def search(term):
             found_tuples.append(data_tuples[i])
     return found_tuples
 
-pprint(search("clustering"))
-
-# Build bag-of-words-model
+# Build bag-of-words-model with a chosen max df (~0.3 works well) to remove stop words,
+# then create feature vectors from data, and build a dictionary to allow O(1) lookup during KNN
 vectorizer = CountVectorizer(max_df = 0.3)
 feature_vectors = vectorizer.fit_transform(data_text).todense()
 vocab_dic = vectorizer.vocabulary_
 
+# Determine frequencies for stop word removal heuristics
 sums = np.sum(feature_vectors, axis=0)
 sorted_word_frequency = -np.sort(-sums)
 sorted_word_frequency = np.asarray(sorted_word_frequency[0]).flatten()
 
+# (Optional) Use pyplot to see removal effect of stop words
 plt.xlabel("Word Rank")
 plt.ylabel("Word Counts")
 plt.title("Word Count vs Work Rank")
 plt.plot(range(len(sorted_word_frequency)), sorted_word_frequency, linestyle="", marker="o")
 
-# Used to search a term or phrase
+# Used to search a term or phrase; main function
 def search_query(term, num_results):
   query = [term]
   query_vector = vectorizer.transform(query)
 
+  # Use KNN with cosine similarity metric, and user-designated k
   neigh = NearestNeighbors(metric="cosine")
   neigh.fit(feature_vectors)
   result = neigh.kneighbors(query_vector, n_neighbors=num_results)
   results = []
   for i in range(num_results):
+    # If no results, alert user; if only less than expected, terminate early
     if i == 0 and result[0][0][0] == 1:
       print("Sorry your search query was not found!")
       return results
@@ -70,4 +83,3 @@ def search_query(term, num_results):
     else:
       results.append(data_tuples[result[1][0][i]])
   return results
-pprint(search_query("text retrieval", 5))
--- scraper.py
+++ scraper.py
@@ -4,42 +4,51 @@ import re
 from bs4 import BeautifulSoup
 from pprint import pprint
 
-filename = "Lecture Video Links.txt.txt"
+###############################################################################
+#
+# Scraper:
+#   Scrapes the course website for associated transcripts and text data, and
+#   stores them in a dump file. Note that scraping url(s) and links are subject
+#   to change and should be updated accordingly should they change.
+#
+###############################################################################
+
+# Clean link data for usage, strip ending char
+filename = "Lecture Video Links.txt"
 with open(filename,"r") as f:
     lines = f.readlines()
-    #print(lines)
     for i in range(len(lines)):
         lines[i] = lines[i].replace("\n","")
 lines[0] = "https://www.coursera.org/lecture/text-retrieval/lesson-1-1-natural-language-content-analysis-rLpwp"
-#print(len(lines))
+
+# *Below are some nuances with the scraping, and some associated problems
 # 47,62 are in a different language
-#https://www.coursera.org/lecture/text-mining/1-5-text-representation-part-1-6T38K
-#https://www.coursera.org/lecture/text-mining/3-2-probabilistic-topic-models-mixture-model-estimation-part-1-QnGYn
+# https://www.coursera.org/lecture/text-mining/1-5-text-representation-part-1-6T38K
+# https://www.coursera.org/lecture/text-mining/3-2-probabilistic-topic-models-mixture-model-estimation-part-1-QnGYn
 # 87 has weird apostrophe problem
-#https://www.coursera.org/lecture/text-mining/6-1-opinion-mining-and-sentiment-analysis-latent-aspect-rating-analysis-part-1-dkntE
+# https://www.coursera.org/lecture/text-mining/6-1-opinion-mining-and-sentiment-analysis-latent-aspect-rating-analysis-part-1-dkntE
 for i in range(len(lines)):
     print(i)
-    if i in [47,62,87]:
+    if i in [47,62,87]:  # Deals with the aforementioned
         continue
+
+    # Parse text data, timestamps, via pattern matching
     url = lines[i]
     request = requests.get(url)
     soup = BeautifulSoup(request.text, "html.parser")
     data = soup.find_all('div', {"class" : "phrases"})
+
     timestamps = soup.find_all('button',{"class" : "timestamp"})
     timestamps_text = []
     for time in timestamps:
         timestamps_text.append(time.text)
+
     data_text = []
     for elem in data:
         data_text.append(elem.text)
     data_tuples = []
-    #for j in range(len(data_text)):
-        #data_tuples.append((timestamps_text[j],data_text[j]))
 
-    with open("data_text1.txt","a") as f:
+    # Write out results to dump file; call this to **overwrite** previous text dump!
+    with open("data_text.txt","w") as f:
         for j in range(len(data_text)):
-            #pprint(data_text[j])
-            #pprint(lines[87])
             f.write(timestamps_text[j] + ":" + data_text[j] + "\n")
-
-#print(data_text[0])
